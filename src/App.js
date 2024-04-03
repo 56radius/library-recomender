cd51f3e521c93c36e8acc28dc9153a357509258a
Merit Mohammed <cussmeritmohammed@gmail.com>
2024-04-03 10:55:32 +0100

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+//importing the main components
+import SplashScreen from "./components/SplashScreen";
+import LoginScreen from "./components/LoginScreen";
+import SignupScreen from "./components/SignupScreen.js";
+
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={showSplash ? <SplashScreen /> : <HomeScreen />}
+          />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<SignupScreen />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
